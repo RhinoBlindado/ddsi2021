181dda4352e2cf406e7adfbee107ff7fd55f99f0
Valentino Lugli <valentinolugli@gmail.com>
2020-12-27 15:43:08 +0100

    [P3] Push Inicial
    Cambios:
    	- Datos migrados del Seminario 2, para reutilizar código.
    		- Renombrado la clase principal a "padelTournamentSystem"
    		- Modificados los valores en el Makefile, adición de la sentencia para limpiar los archivos *.class
    		- Añadido el esqueleto del Subsistema 2

---
+++ practica3/usuariosEntradas.java
@@ -0,0 +1,30 @@
+/*
+    Titulo:         Subsistema 2 - Usuarios / Entradas
+    Fecha:          Diciembre 2020
+    Autor:          Valentino Lugli
+    Descripción:    Implementación de parte del Subsistema 2 - Usuarios / Entradas, la funcionalidad de Añadir Entradas a Compra
+*/
+
+// Librerias
+import java.sql.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.*; 
+
+// Clase Base
+
+public class usuariosEntradas 
+{
+
+    public static void annadirEntradasACompra(Connection conn) throws SQLException
+    {
+        
+    }
+
+    public static void main(String args)
+    {
+        System.out.print(args);
+    }
+}
