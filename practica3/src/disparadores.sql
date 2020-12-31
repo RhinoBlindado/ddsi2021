821fcc4a812c80d8f12d9581842c83042978d0b3
elaypunto <elagomezmorales@gmail.com>
2020-12-31 20:52:37 +0100

    Añado disparador Patrocinadores/Colaboradores, supuestamente funciona pero da un error raro

--- practica3/src/disparadores.sql
+++ practica3/src/disparadores.sql
@@ -1,1 +1,14 @@
- 
+-- Disparador para que no se permita que una entidad colaboradoradora sea patrocinadora en una misma edición
+
+CREATE OR REPLACE TRIGGER EntidadUnicaPorEdicion 
+	BEFORE 
+	INSERT ON patrocina
+	FOR EACH ROW
+DECLARE
+	colaboradores_edicion INTEGER;
+BEGIN
+	SELECT count(*) INTO colaboradores_edicion FROM colabora WHERE idEntidad = :new.idEntidad AND anno = :new.anno;
+	IF (colaboradores_edicion > 1) THEN 
+		raise_application_error(-20600, :new.idEntidad || 'Un colaborador no puede ser patrocinador en la misma edición');
+	END IF;
+END; 
