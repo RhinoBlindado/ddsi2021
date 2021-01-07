96bbefc34cc56e3ea3fda4c4b055eb046ad168bc
Valentino Lugli <valentinolugli@gmail.com>
2021-01-07 12:34:57 +0100

    [P3] Disparador Subsistema 2 Funcional

--- practica3/src/disparadorSubsistema2.sql
+++ practica3/src/disparadorSubsistema2.sql
@@ -7,10 +7,13 @@ CREATE OR REPLACE TRIGGER unaCompraPorDia
 	FOR EACH ROW
 
 DECLARE
-	sameDay INTEGER;
+	sameDayCount INTEGER;
+    idUsr INTEGER;
 BEGIN
-	SELECT count(*) INTO sameDay FROM COMPRA_FINALIZADA WHERE TO_CHAR(FECHAFIN,'DD-MM-YYYY') = TO_CHAR(:new.FECHAFIN,'DD-MM-YYYY');
-	IF (sameDay > 1) THEN
-		raise_application_error(-666, :new.FECHAFIN || 'Un usuario no puede realizar más de una compra por día.');
+    SELECT idusuario INTO idUsr FROM COMPRA_REALIZA_INICIA WHERE idcompra = :new.idcompra;
+    SELECT COUNT(*) INTO sameDayCount FROM COMPRA_FINALIZADA WHERE TO_CHAR(FECHAFIN,'DD-MM-YYYY') = TO_CHAR(:new.FECHAFIN,'DD-MM-YYYY') 
+        AND idcompra IN (SELECT idcompra FROM COMPRA_REALIZA_INICIA WHERE idUsuario = idUsr);
+	IF (sameDayCount > 1) THEN
+		raise_application_error(-20666,'Usuario' || idUsr || 'ha intentado realizar más de una compra por día: ' || :new.FECHAFIN);
 	END IF;
 END; 
