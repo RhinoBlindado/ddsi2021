801d80a2b50705555e50cb34633517490ef9e2f1
Valentino Lugli <valentinolugli@gmail.com>
2021-01-07 11:35:02 +0100

    [Refactor]

--- practica3/src/disparadorSubsistema3.sql
+++ practica3/src/disparadorSubsistema3.sql
@@ -1,21 +1,3 @@
--- Subsistema 2 - Usuarios / En
--- Disparador para evitar que se realice más de una compra al día
-
-CREATE OR REPLACE TRIGGER unaCompraPorDia
-	BEFORE
-	INSERT ON COMPRA_FINALIZADA
-	FOR EACH ROW
-
-DECLARE
-	sameDay INTEGER;
-BEGIN
-	SELECT count(*) INTO sameDay FROM COMPRA_FINALIZADA WHERE TO_CHAR(FECHA_PAGO,'DD-MM-YYYY') = TO_CHAR(:new.FECHA_PAGO,'DD-MM-YYYY');
-	IF (sameDay > 1) THEN
-		raise_application_error(-666, :new.FECHA_PAGO || 'Un usuario no puede realizar más una compra por día.')
-	END IF;
-END;
-
-
 -- Disparador para que no se permita que una entidad colaboradoradora sea patrocinadora en una misma edición
 
 CREATE OR REPLACE TRIGGER EntidadUnicaPorEdicion 
