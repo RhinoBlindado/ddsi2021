0b484c2543df60f48c94947b68f500bf5d07d6fe
KTJota <52372246+KTJota@users.noreply.github.com>
2021-01-15 18:02:41 +0100

    Update disparadorSubsistema4.sql

--- practica3/src/disparadorSubsistema4.sql
+++ practica3/src/disparadorSubsistema4.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE TRIGGER horarioDigno
     UPDATE OF horas ON trabaja
     FOR EACH ROW
 BEGIN
-    IF (horas > 8) THEN
-        raise_application_error(-20600, horas || ' un trabajador no puede trabajar mas de 8 horas');
+    IF (:new.horas > 8) THEN
+        raise_application_error(-20600, :new.horas || ' un trabajador no puede trabajar mas de 8 horas');
     END IF;
 END;
