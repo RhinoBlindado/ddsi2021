1e949f73fd141ff2e511d29a22dc0deecdb37f15
elaypunto <elagomezmorales@gmail.com>
2021-01-15 18:36:30 +0100

    Makefile y Personal/Horarios actualizados

--- practica3/personalHorarios.java
+++ practica3/personalHorarios.java
@@ -16,13 +16,63 @@ import java.lang.*;
 // Clase Base
 public class personalHorarios {
     // Metodos Privados
+
+	 /**
+     * @param conn Objeto que proporciona el vínculo entre la base de datos y la
+     *             aplicación en java
+     * @brief Muestra todas las Ediciones registradas
+     */
+    private static void mostrarEdiciones(Connection conn) throws SQLException {
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM EDICION");
+
+        while (rs.next()) {
+            System.out.println("Anno:" + rs.getString("ANNO"));
+        }
+    }
+
+ /**
+     * @param conn Objeto que proporciona el vínculo entre la base de datos y la
+     *             aplicación en java
+     * @return vectorAnno, ArrayList de entero que almacena los años de las
+     *         ediciones registradas
+     * @brief Guarda las Ediciones en un ArrayList
+     */
+    private static ArrayList<Integer> annoEdiciones(Connection conn) throws SQLException {
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM EDICION");
+
+        ArrayList<Integer> vectorAnno = new ArrayList<>();
+
+        while (rs.next()) {
+            vectorAnno.add(rs.getInt("ANNO"));
+        }
+        return vectorAnno;
+    }
+
     
     public static void mostrarPersonalNoTrabaja(Connection conn) throws SQLException {
+		Scanner scan = new Scanner(System.in);
         Statement st = conn.createStatement();
         int anno;
+		System.out.println("\n>>Ediciones en las que la Entidad podria Patrocinar: ");
+        mostrarEdiciones(conn);
+
         System.out.println(">>>Introduce el anno del que obtener el personal que no trabaja: ");
         anno = scan.nextInt();
-        ResultSet rs = ("SELECT * FROM PERSONAL WHERE NOT EXISTS(SELECT trabaja.idPersonal FROM TRABAJA WHERE trabaja.idPersonal = Personal.idPersonal AND trabaja.anno ="+anno+ ")");																															                                     
+
+		ArrayList<Integer> vectorEdiciones = annoEdiciones(conn);
+
+        while (!vectorEdiciones.contains(anno)) {
+        	System.out.println(
+                                "\n>>>ERROR al introducir la edición, estas son las ediciones en las que la Entidad podría patrocinar: ");
+            mostrarEdiciones(conn);
+
+           	System.out.print("\n>>Vuelva a insertar Edicion en la que la Entidad Patrocina: ");
+            anno = scan.nextInt();
+        }
+		
+        ResultSet rs = st.executeQuery("SELECT * FROM PERSONAL WHERE NOT EXISTS(SELECT trabaja.idPersonal FROM TRABAJA WHERE trabaja.idPersonal = Personal.idPersonal AND trabaja.anno ="+anno+ ")");																															                                     
         while (rs.next()) {
             System.out.println(
                     "\nID Personal:" + rs.getString("IDPERSONAL") + " Correo: " + rs.getString("CORREO")
